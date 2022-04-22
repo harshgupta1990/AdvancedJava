21eb5fd38fd0acff82f75801a5b98ca850698d03
harshitgupta <harshgupta1990@gmail.com>
2022-04-22 12:01:44 +0530

    JavaDocker Simple Helloworrld project

---
+++ JavaDocker/src/com/myapp/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.myapp;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World this docker running from eclipse");
+
+	}
+
+}
